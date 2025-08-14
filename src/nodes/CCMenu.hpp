80b451234b8233b30f794978d83179f74df2636d
Alphalaneous <38200084+Alphalaneous@users.noreply.github.com>
2025-08-14 11:27:55 -0400

    Update CCMenu.hpp

--- src/nodes/CCMenu.hpp
+++ src/nodes/CCMenu.hpp
@@ -90,7 +90,7 @@ class $nodeModify(MyCCMenu, cocos2d::CCMenu) {
         auto self = reinterpret_cast<CCMenu*>(this);
         if (auto item = self->m_pSelectedItem) {
             auto realItem = typeinfo_cast<CCMenuItemSpriteExtra*>(item);
-            if (!realItem) continue;
+            if (!realItem) return;
             if (EventCCMenuItemSpriteExtra* button = static_cast<EventCCMenuItemSpriteExtra*>(realItem)) {
                 button->checkTouch(!button->isSelected());
             }
